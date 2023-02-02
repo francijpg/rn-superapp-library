baa4cbf865fd8d1ce15d76b34340628400100cd0
Francisco Plaza <francisco.plaza@thoughtworks.com>
2023-02-02 15:57:33 -0300

    chore: first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {TestScreen} from './src';
+
+function App(): JSX.Element {
+  return <TestScreen />;
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import TestScreen from './TestScreen';
+import TestScreen2 from './TestScreen2';
+
+export {TestScreen, TestScreen2};
